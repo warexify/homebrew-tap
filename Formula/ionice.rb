5d86ecb19150f7ac8e910f3930eefed094312393
Eugene Istratov <warexify@outlook.com>
2018-08-04 17:36:08 +0300

    Update

--- Formula/ionice.rb
+++ Formula/ionice.rb
@@ -1,16 +1,18 @@
 class Ionice < Formula
   desc "ionice for macOS"
-  homepage "https://github.com/warexify/ionice"
-  url "https://github.com/warexify/ionice/archive/v0.1.tar.gz"
+  homepage "https://www.kernel.org/pub/linux/utils/util-linux"
+  url "https://github.com/someposer/ionice/archive/v0.1.tar.gz"
   version "0.1"
   sha256 "4fe17adb0346e088ead6630c7eb2d7cb815833b207306323af9f52aae1151a12"
 
-  head "https://github.com/warexify/ionice.git"
+  head "https://github.com/someposer/ionice.git"
 
   def install
-    system "make"
+    system "make", "ionice"
+    system "make", "ionice_fork"
     bin.mkpath
     bin.install "ionice"
+    bin.install "ionice_fork"
   end
 
   test do
