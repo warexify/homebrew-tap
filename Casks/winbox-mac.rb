478aa14cea93fbec38bcae1ad9f6d142e2f80d59
Eugene Istratov <warexify@outlook.com>
2018-08-11 10:48:01 +0300

    Update winbox

--- Casks/winbox-mac.rb
+++ Casks/winbox-mac.rb
@@ -1,6 +1,6 @@
 cask 'winbox-mac' do
-  version '3.16'
-  sha256 'fbd43f8d5dc68079a4e80b8a09196b53bc6a2e9544df4f29b9c820502e7a7168'
+  version '3.17'
+  sha256 '474079ae9f27c48c2be6733b5680c90cd2eeefc9fbdd5ede1b99c64629963594'
 
   url "https://github.com/nrlquaker/winbox-mac/releases/download/v#{version}/Winbox-mac-#{version}.zip"
   appcast 'https://github.com/nrlquaker/winbox-mac/releases.atom'
