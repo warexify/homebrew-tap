2f0853dd6fbbe279e6b3e75a3efe770a482f0ddf
Eugene Istratov <warexify@outlook.com>
2018-08-16 09:47:19 +0300

    Update wxFormBuilder

--- Casks/wxformbuilder.rb
+++ Casks/wxformbuilder.rb
@@ -1,10 +1,10 @@
 cask 'wxformbuilder' do
-  version '3.8.0'
-  sha256 'fa252cf657e8168c4dcf2bcb36c0fd254334cc79a7b9b537cbb1e0e4b675ca03'
+  version '3.8.1'
+  sha256 '23c761b42c73a20443e1c0e87a07d114cacbc11f81ab70385b053384aa7512cb'
 
   # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/wxFormBuilder/wxFormBuilder/releases/download/v3.8.0/wxFormBuilder_macOS.zip'
-  name 'wxCrafter'
+  url "https://github.com/wxFormBuilder/wxFormBuilder/releases/download/v#{version}/wxFormBuilder_macOS.zip"
+  name 'wxFormBuilder'
   homepage 'http://wxformbuilder.org/'
 
   app 'wxFormBuilder.app'
