24505a334ac94c52b276404bd1a91c77e6496c9c
Eugene Istratov <warexify@outlook.com>
2018-11-15 16:04:40 +0300

    Add acpica 20181031

---
+++ Formula/acpica.rb
@@ -0,0 +1,19 @@
+class Acpica < Formula
+  desc "OS-independent implementation of the ACPI specification"
+  homepage "https://www.acpica.org/"
+  url "https://acpica.org/sites/acpica/files/acpica-unix-20181031.tar.gz"
+  sha256 "090e75cf2a6bc53dc93a82822ae74962942bc66d3932a00ed130e640b3a3e1ff"
+  head "https://github.com/acpica/acpica.git"
+
+  bottle :unneeded
+
+  def install
+    ENV.deparallelize
+    system "make", "PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/acpihelp", "-u"
+  end
+end
