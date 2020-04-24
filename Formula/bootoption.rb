293c1e6edea1fd7e097aca69fdf73160183c0a6c
Eugene Istratov <warexify@outlook.com>
2020-04-25 00:56:17 +0300

    Update

--- Formula/bootoption.rb
+++ Formula/bootoption.rb
@@ -1,16 +1,16 @@
 class Bootoption < Formula
-  desc "EFI boot menu manipulation for macOS, like efibootmgr"
-  homepage "https://github.com/vulgo/bootoption/"
-  url "https://github.com/vulgo/bootoption/archive/v0.2.99.tar.gz"
-  sha256 "d9dd8f5e063504e076f03fa0b455b26c730bd8f7633ca30ec2beb06734e4bb71"
-  head "https://github.com/vulgo/bootoption.git"
+  desc "EFI boot menu manipulation for macOS"
+  homepage "https://github.com/bootoption/bootoption/"
+  url "https://github.com/bootoption/bootoption/archive/0.4.0.tar.gz"
+  sha256 "284f5411529bee580f5bfdf28e91db565b7fff8f78ded06061b5d321853fab3a"
+  head "https://github.com/bootoption/bootoption.git"
 
   bottle :unneeded
 
   depends_on :xcode => ["9.0", :build]
 
   def install
-    system "swift", "build", "--disable-sandbox", "--configuration", "release", "-Xswiftc", "-target", "-Xswiftc", "x86_64-apple-macosx10.12"
+    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc", "-static-stdlib"
     bin.install ".build/release/bootoption"
     man.install Dir["man/*"]
   end
--- Formula/csrstat.rb
+++ Formula/csrstat.rb
@@ -1,9 +1,9 @@
 class Csrstat < Formula
   desc "Command line tool to get the active System Integrity Protection status"
   homepage "https://github.com/Piker-Alpha/csrstat"
-  url "https://github.com/vulgo/csrstat/archive/v1.8.tar.gz"
+  url "https://github.com/warexify/csrstat/archive/v1.8.tar.gz"
   sha256 "0ae4d31b5d1eef60ac7644651017df7296a2ed7575cec19a7b587d25a7c0d3f1"
-  head "https://github.com/Piker-Alpha/csrstat.git"
+  head "https://github.com/warexify/csrstat.git"
 
   bottle :unneeded
 
