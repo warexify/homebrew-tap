9ca04d52d873c2aa3fb91ad74a060e86ac17f7f6
Evgeny Istratov <warexify@outlook.com>
2021-09-26 17:19:53 +0700

    Update ocat

--- Casks/qtopencoreconfig.rb
+++ Casks/qtopencoreconfig.rb
@@ -1,6 +1,6 @@
 cask "qtopencoreconfig" do
-  version "20210925"
-  sha256 "a96f2917d8d3a2a00ee5cf6cb8b2fa4c3d56a4a682e6fb03535ca1102030beb4"
+  version "20210926"
+  sha256 "335e64f8ea327792eb2567bcfc95288d4ff76001b626b9f4180c3dc074a18e19"
 
   url "https://github.com/ic005k/QtOpenCoreConfig/releases/download/#{version}/OCAT_Mac.dmg"
   name "QtOpenCoreConfig"
