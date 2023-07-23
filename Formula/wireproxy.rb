d8bce160db0aa3e0ec7d248e65959f982c5cb295
Evgeny Istratov <warexify@outlook.com>
2023-07-23 07:33:31 +0700

    Update wireproxy

--- Formula/wireproxy.rb
+++ Formula/wireproxy.rb
@@ -1,10 +1,10 @@
 class Wireproxy < Formula
   desc "Wireguard client that exposes itself as a socks5 proxy or tunnels"
-  homepage "https://github.com/octeep/wireproxy"
-  url "https://github.com/octeep/wireproxy/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "3779a157713b462a7b5322f1437f768ab69ae446df0ceca76c6dff7c4030e425"
+  homepage "https://github.com/pufferffish/wireproxy"
+  url "https://github.com/octeep/wireproxy/archive/refs/tags/v1.0.6.tar.gz"
+  sha256 "28f20b2387a1266dc56f204deea7850e3052f75684062d1fa3f3f7c9204c97b5"
   license "ISC"
-  head "https://github.com/octeep/wireproxy.git", branch: "master"
+  head "https://github.com/pufferffish/wireproxy.git", branch: "master"
 
   depends_on "go" => :build
 
